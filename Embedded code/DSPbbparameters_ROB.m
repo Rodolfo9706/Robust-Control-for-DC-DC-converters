5eec684bf1893605a1cc306b2c7945d9fd9d23e8
Rodolfo Isaac Verdín <58195148+Rodolfo9706@users.noreply.github.com>
2026-04-27 12:09:51 -0600

    Add files via upload

--- Embedded code/DSPbbparameters_ROB.m
+++ Embedded code/DSPbbparameters_ROB.m
@@ -5,14 +5,21 @@ C  = 2610*10^-6    ;%470*10^-6  ;%220*10^-6
 R  = 1000000 ; %10
 L  = 2010*10^-6 ;
 
-x2d = -18;
+x2d = -6;
 
 %control parameters
 
 %delta1 = 0.2;
 %delta2 = 0.1;
-k1 = 4500;  %2000; %8000;   %en simu 0.925                     PARA EL CTRL IMPLEMTADO EN LA DSP 0.2
-k2 = 80;  %8000;%5;    %16                  PARA EL CTRL IMPLEMTADO EN LA DSP 20
+
+%ganancias control viejo octubre 24
+k1 = 3000;  %2000; %8000;   %en simu 0.925                     PARA EL CTRL IMPLEMTADO EN LA DSP 0.2
+k2 = 10;  %8000;%5;    %16               PARA EL CTRL IMPLEMTADO EN LA DSP 20
+
+%Ganancias con el nuevo control
+%k1 = 0;  %2000; %8000;   %en simu 0.925                     PARA EL CTRL IMPLEMTADO EN LA DSP 0.2
+%k2 = 0;  %8000;%5;    %16
+
 %k=1000;    %10000                 PARA EL CTRL IMPLEMTADO EN LA DSP 1000
 %vd = -10;
 Ts=0.01;
